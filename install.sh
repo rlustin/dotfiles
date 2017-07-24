81ba19cc9473470d46c4f92c23ec327aa226fadb
Raphaël Lustin <raphael@lustin.fr>
2017-07-24 16:42:16 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -f -s $(pwd)/.config ~/.config
+ln -f -s $(pwd)/.gitconfig ~/.gitconfig
+ln -f -s $(pwd)/.gitconfig-tbk ~/.gitconfig-tbk
+ln -f -s $(pwd)/.global_gitignore ~/.global_gitignore
+ln -f -s $(pwd)/.vim ~/.vim
+ln -f -s $(pwd)/.zshrc ~/.zshrc
+
